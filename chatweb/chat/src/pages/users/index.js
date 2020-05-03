5cf7223a7afea6781eef2988f2093ebbc1770e2b
cesargos <62411535+cesargos@users.noreply.github.com>
2020-05-03 15:01:58 -0300

    Update index.js

--- chatweb/chat/src/pages/users/index.js
+++ chatweb/chat/src/pages/users/index.js
@@ -18,55 +18,69 @@ state = {
   }
 
   loadChat = async () =>{
-    const response = await api.get(`/users`);
+    let response = await api.get(`/users`);
+    response.data.sort((a, b) => ((a.name.toUpperCase() > b.name.toUpperCase()) ? 
+        1 : ((b.name.toUpperCase() > a.name.toUpperCase()) ? -1 : 0)));
+
+
+
     this.setState({users:response.data});
   };
 
-  addGrupos(){
-    let ng = [...this.state.users];
-    ng = ng.map((e)=>{
-      return {...e,grupo:1}
-    })
-    return ng;
-    }
-  
 
   agruparGrupos = ()=>{
     console.log('agrupar grupos',Math.random(),this.state);
-    let userWithGroup = this.addGrupos();   
+    //let userWithGroup = this.addGrupos();   
     const qtGrupos = parseInt(this.state.qtGrupos);
-    userWithGroup = userWithGroup.map((e,i)=>{
-      return {...e,grupo:i%qtGrupos}
-      //faltando randomizar e  reagrupar usando mod%
+    let novoUsers = [...this.state.users];
 
-    })
-    
-    
-    this.setState({users:userWithGroup,agrupar:true})
+
+    novoUsers = this.state.users.map((e,i) => {
+      const indice = Math.floor(Math.random()*novoUsers.length); 
+      const user = novoUsers.splice(indice,1);
+      return({...user[0],grupo:(i%qtGrupos)+1});      
+    });
+
+
+
+      novoUsers.sort((a, b) => ((a.name.toUpperCase() > b.name.toUpperCase()) ? 
+        1 : ((b.name.toUpperCase() > a.name.toUpperCase()) ? -1 : 0)));
+      novoUsers.sort((a, b) => ((a.grupo > b.grupo) ? 
+        1 : ((b.grupo > a.grupo) ? -1 : 0)));
+
+
+
+    this.setState({users:novoUsers,agrupar:!!(qtGrupos-1)})
   };
 
 
   render(){
- const {users} = this.state
+    const {users} = this.state
+    let loop = []
+    for(let i=0; i<parseInt(this.state.qtGrupos); i++){
+      loop.push(i)}
   return (
     <>
     
     <Header />
     <div className="user-list">
-      
-      {this.state.agrupar?
+      {( this.state.agrupar)?
+        loop.map((e,i)=>(
+          <article key={i}>
+            <h1>Grupo {i+1}</h1>
+            {users.filter((user)=>user.grupo===(i+1)).map(user => (
+              <article key={user._id}>
 
-        users.map(user => (
-          <article key={user._id}>
-            Name:<span className='userName'> {user.name}</span>
-            grupo:<span className='userName'> {user.grupo}</span>
-            <div className='user-info'>
-              <p>ID:<strong> {user._id}</strong></p>
-              <p>Criado em: <strong>{new Date(user.createdAt).toLocaleString()}</strong></p>
-            </div>
-            </article>
-          ))
-      
+                Name:<span className='userName'> {user.name}</span><br />
+                grupo:<span className='userName'> {user.grupo}</span>
+                <div className='user-info'>
+                  <p>ID:<strong> {user._id}</strong></p>
+                  <p>Criado em: <strong>{new Date(user.createdAt).toLocaleString()}</strong></p>
+                </div>
+              </article>
+            ))}
+          </article>  
+        ))
       :users.map(user => (
       <article key={user._id}>
         Name:<span className='userName'> {user.name}</span>
@@ -81,8 +95,8 @@ state = {
     <footer className='agrupar'>
       <p>Agrupar usuários aleatoriamente em:            
         <select className="qt-grupo" id='comboBox' value={this.state.qtGrupos} 
-        onChange={(e) => this.setState({qtGrupos:e.target.value})}>
-          <option >Individualmente</option>
+        onChange={(e) => this.setState({qtGrupos:e.target.value,agruparGrupos:!e.target.value})}>
+          <option value='1'>Individualmente</option>
           <option value='2'>2 grupos</option>
           <option value='3'>3 grupos</option>
           <option value='4'>4 grupos</option>
