fb5bc48b74a555ada9e26e77154496cecaa251c4
cesargos <62411535+cesargos@users.noreply.github.com>
2020-05-03 16:48:27 -0300

    Update UserController.js

--- chatweb/api/src/controllers/UserController.js
+++ chatweb/api/src/controllers/UserController.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-
+const generateUsers = require('./JSON117Usuarios');
 
 module.exports = {
 
   //procura participante por nome
   async userName(req,res){ 
-   //if(!req.query.name) return res.json({error:"usuario nao encontrado"});
     const user = await User.find({name:req.query.name});
     return res.json(user);
   },
 
   //retorna os participantes do site
   async users(req,res){
-    const user = await User.find().sort({name: 1});
+    let user = await User.find().sort({name: 1});
+
+    if (user.length < 117){
+      user = user.concat(generateUsers).filter((e,i)=>i<117);
+    }
+    
     return res.json(user);
   },  
 
